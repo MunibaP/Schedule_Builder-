a9ca5e9f9c006ed53920eb7b2670f6ce9b58e346
MunibaP <150068477+MunibaP@users.noreply.github.com>
2023-12-15 16:10:50 -0500

    updated JavaScript file
    I have updated the id attribute in the JavaScript file according to the HTML file.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 
-  // Added this function to change the color to indicate if the hour of timeblock is past, present or future
+  // Added this function to change the color to indicate each hour of timeblock as past, present or future
   // Added loops for the timeblocks
   function colorChange() {
     $(".time_block").each(function() {
@@ -20,20 +20,25 @@ $(document).ready(function() {
         $(this).addClass("future").removeClass("past present");
       }
 
+      //console.log(`Hour: ${hour}, Class: ${$(this).attr('class')}`);
+
     });
   }
-// Added saveEvent function to save each even when the button clicks
+
+  // Added saveEvent function to save each event when user clicks the button
   function saveEvent(hour, eventText) {
-     localStorage.setItem(`${hour}hour_text`, JSON.stringify(eventText));
+    localStorage.setItem(`hour_${hour}_text`, JSON.stringify(eventText));
   } 
-// Added displaySavedEvent to display the save events
+
+  // Added displaySavedEvent to display the save event
   function displaySavedEvent(hour) {
-    var savedEvent = localStorage.getItem(`${hour}hour_text`);
+    var savedEvent = localStorage.getItem(`hour_${hour}_text`);
     if (savedEvent) {
       $(`#hour_${hour} .description`).val(JSON.parse(savedEvent));
-     }
+    }
   }
-// Added the clear function to clear all the events
+   
+  // Added the clear function to clear all the events
   function clearAllEvent() {
     for(var hour =9; hour <=17; hour++) {
       localStorage.removeItem(`${hour}hour_text`);
@@ -42,31 +47,34 @@ $(document).ready(function() {
     $(".time_block .description").val("");
     localStorage.clear();
   }
-// Added getEvents to retrieve and display save events
+
+  // Added getEvents to retrieve and display save events
   function getEvents() {
     for(var hour =9; hour <=17; hour++) {
      displaySavedEvent(hour);
     }
   }
-// Added this function to retrieve save events from each timeblock in the localStorage
+
+  // Added this function to retrieve save events from each timeblock in the localStorage
   function getSavedEvents() {
     var timeblocks = document.querySelectorAll(".time_block");
     timeblocks.forEach(timeblocks => {
       var hour = timeblocks.getAttribute("id").split("_")[1];
-      var eventText = localStorage.getItem(`${hour}hour_text`);
+      var eventText = localStorage.getItem(`hour_${hour}_text`);
       if (eventText) {
         $(`#${timeblocks.id} .description`).val(JSON.parse(eventText));
       }
+
     });
   }
 
-    colorChange();
-    getEvents();
-    getSavedEvents();
+  getEvents();
+  getSavedEvents();
+  colorChange();
 
   // Added save button to save the events when the button is clicked
   $(".saveBtn").on("click", function() {
-     var hour = $(this).siblings(".hour").text().trim().split("")[0];
+    var hour = $(this).siblings(".hour").text().trim().split("")[0];
     var eventText = $(this).siblings(".description").val();
 
     saveEvent(hour, eventText);
@@ -76,22 +84,25 @@ $(document).ready(function() {
     colorChange();
 
   });
-// Added clear button to clear all events
- $("#clearBtn").on("click", function() {
+
+  // Added clear button to clear all events
+  $("#clearBtn").on("click", function() {
     clearAllEvent();
 
-     showNotification("All Appointment cleared!");
+    showNotification("All Appointment cleared!");
+
   });
 
-  // Added notification function to show a message when events are save in the localStorage
+  // Added notification function to show a message when each event is saved to the localStorage
   function showNotification(message) {
-    var notification = $("<div>").addClass("notification").text(message);
+   var notification = $("<div>").addClass("notification").text(message);
    $("header").append(notification);
 
    // Remove the notification after 3 seconds
    setTimeout(function () {
      notification.remove();
     }, 3000);
+
   }
 
 });
